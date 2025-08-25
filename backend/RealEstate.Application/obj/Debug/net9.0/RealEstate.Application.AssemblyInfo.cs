97095de618c6fcbdefd7b92bf9d6ed6547f67ed6
DiegoBallesterosP <diegopulido-951@hotmail.com>
2025-08-25 08:30:04 -0500

    Clean code

--- backend/RealEstate.Application/obj/Debug/net9.0/RealEstate.Application.AssemblyInfo.cs
+++ backend/RealEstate.Application/obj/Debug/net9.0/RealEstate.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658d3b1eae3b6e11039149e7450fa8ed3e72ae3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b79963bf90f23a2e87b594f53c01506d901e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
